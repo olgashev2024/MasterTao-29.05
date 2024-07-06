ee2e792115fbc3aec9861c28363b8dbf33f6198a
Olga Shevtsova <shevolga92@gmail.com>
2024-07-06 12:00:58 +0300

    header mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -3,7 +3,9 @@ const mobileContainer = document.querySelector('.mobile-container');
  
 
   function showBurgerMenu() {
-    mobileContainer.classList.toggle('show')
+    mobileContainer.classList.toggle('show');
+    navigation.classList.toggle('show');
+  
   }
 
   burger.addEventListener('click', showBurgerMenu)
