6723ef85f600940023a252c3af9c47f58b0c9439
Olga Shevtsova <shevolga92@gmail.com>
2024-07-08 20:57:56 +0300

    first-screen desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -4,8 +4,23 @@ const mobileContainer = document.querySelector('.mobile-container');
 
   function showBurgerMenu() {
     mobileContainer.classList.toggle('show');
-    navigation.classList.toggle('show');
-  
+    burger.classList.toggle('is-active');
   }
 
   burger.addEventListener('click', showBurgerMenu)
+
+
+  const swiper = new Swiper('.swiper-info', {
+    loop:true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
+    },
+  });
